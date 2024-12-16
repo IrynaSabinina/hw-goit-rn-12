9d764386b669c843c61e713c1ba4efbc3d6dc16a
Iryna <100072177+IrynaSabinina@users.noreply.github.com>
2024-12-16 09:04:37 +0100

    add nav lib

--- App.js
+++ App.js
@@ -1,17 +1,21 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
 import RegistrationScreen from "./Screens/Auth/RegistrationScreen";
+
 import LoginScreen from "./Screens/Auth/LoginScreen";
+import { NavigationContainer } from "@react-navigation/native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      {/* <LoginScreen /> */}
-      <RegistrationScreen />
-      {/* <Text>Open up App.js to start working on your app!</Text> */}
+    <NavigationContainer>
+      <View style={styles.container}>
+        {/* <LoginScreen /> */}
+        <RegistrationScreen />
+        {/* <Text>Open up App.js to start working on your app!</Text> */}
 
-      <StatusBar style="auto" />
-    </View>
+        <StatusBar style="auto" />
+      </View>
+    </NavigationContainer>
   );
 }
 
