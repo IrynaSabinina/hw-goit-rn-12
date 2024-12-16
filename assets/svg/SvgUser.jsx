416650a56a6dc1de86b00ddb5ba7b9c0702e93e4
Iryna <100072177+IrynaSabinina@users.noreply.github.com>
2024-12-16 15:52:35 +0100

    add all svgs

--- assets/svg/SvgUser.jsx
+++ assets/svg/SvgUser.jsx
@@ -2,22 +2,23 @@ import * as React from 'react';
 import Svg, { Path } from 'react-native-svg';
 
 
-const SvgLocation = props => (
+const SvgUser = props => (
   <Svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} fill="none" {...props}>
     <Path
-      stroke="#BDBDBD"
+      stroke={props.stroke}
       strokeLinecap="round"
       strokeLinejoin="round"
-      d="M20 10.364C20 16.09 12 21 12 21s-8-4.91-8-10.636C4 6.297 7.582 3 12 3s8 3.297 8 7.364v0Z"
-      clipRule="evenodd"
+      strokeOpacity={0.8}
+      d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"
     />
     <Path
-      stroke="#BDBDBD"
+      stroke={props.stroke}
       strokeLinecap="round"
       strokeLinejoin="round"
-      d="M12 14a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"
+      strokeOpacity={0.8}
+      d="M12 11a4 4 0 1 0 0-8 4 4 0 0 0 0 8Z"
       clipRule="evenodd"
     />
   </Svg>
 );
-export default SvgLocation;
+export default SvgUser;
